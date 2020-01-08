2a8ea2c301bf2073ef30675ec6d0d7b1c1f032b9
Wenping Guo <ybyygu@gmail.com>
2020-01-08 17:08:28 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+// lib.rs
+// :PROPERTIES:
+// :header-args: :tangle src/lib.rs
+// :END:
+
+// [[file:~/Workspace/Programming/gchemol-rs/gchemol-readwrite/gchemol-readwrite.note::*lib.rs][lib.rs:1]]
+
+// lib.rs:1 ends here
