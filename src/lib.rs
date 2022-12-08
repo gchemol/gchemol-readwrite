5a064e2ae8a1dbf7f321625a8b8a01827ac17627
Wenping Guo <ybyygu@gmail.com>
2022-12-08 20:13:08 +0800

    Fix license setting issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// [[file:../gchemol-readwrite.note::*header][header:1]]
+// [[file:../gchemol-readwrite.note::0c98a9cf][0c98a9cf]]
 //===============================================================================#
 //   DESCRIPTION:  basic read & write support for molecular file
 //
@@ -8,9 +8,8 @@
 //        AUTHOR:  Wenping Guo <ybyygu@gmail.com>
 //       LICENCE:  GPL version 3
 //       CREATED:  <2018-04-11 Wed 15:42>
-//       UPDATED:  <>
 //===============================================================================#
-// header:1 ends here
+// 0c98a9cf ends here
 
 // [[file:../gchemol-readwrite.note::*mods][mods:1]]
 // ignore compiler warnings due to nom macro uses
